faeaa709a5451eb1b7c33d2f06f0d65851cd772c
Victor Petrillo <17276460+Zeekeybookeydo@users.noreply.github.com>
2022-07-16 12:10:04 -0400

    Basic Commands

--- src/bot.js
+++ src/bot.js
@@ -5,11 +5,23 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [
+	Intents.FLAGS.GUILDS,
+	Intents.FLAGS.GUILD_MESSAGES] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+client.on('messageCreate', (message) => {
+	console.log(`[${message.author.tag}]: ${message.content}`);
+	if (message.content === 'hello') {
+		message.reply('Hello there');
+	}
+	else if (message.content === 'ping') {
+		message.reply('pong');
+	}
 });
 
 // Login to Discord with your client's token
